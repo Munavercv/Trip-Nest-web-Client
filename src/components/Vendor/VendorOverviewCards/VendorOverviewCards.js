cc6c060d67a1f8befea604056463f37aeff197fd
Munavercv <munavermunu80@gmail.com>
2025-01-27 16:36:03 +0530

    counts for vendor overview cards

--- src/components/Vendor/VendorOverviewCards/VendorOverviewCards.js
+++ src/components/Vendor/VendorOverviewCards/VendorOverviewCards.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './VendorOverviewCards.module.css'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
+import { useSelector } from 'react-redux'
 
 const VendorOverviewCards = () => {
+    const { user } = useSelector(state => state.auth)
+
+    const [pendingbookingCount, setPendingbookingCount] = useState(0)
+    const [activePackagesCount, setActivePackagesCount] = useState(0);
+
+    const fetchPendingbookingCount = async () => {
+        const response = await axios.get(`/api/vendor/pending-booking-count/${user.userId}`)
+        setPendingbookingCount(response.data.count)
+    }
+
+    const fetchActivePackageCount = async () => {
+        const response = await axios.get(`/api/vendor/active-package-count/${user.userId}`)
+        setActivePackagesCount(response.data.count)
+    }
+
+    useEffect(() => {
+        if (user) {
+            fetchPendingbookingCount()
+            fetchActivePackageCount()
+        }
+    }, [user])
+
     return (
         <section className={`${styles.cardsSec} container-fluid px-lg-5 px-3 py-4`} >
 
@@ -12,7 +36,7 @@ const VendorOverviewCards = () => {
                     <div className={`${styles.card} card`}>
                         <div className="card-body text-center">
                             <h5 className="card-title">Bookings</h5>
-                            <h6 className="card-subtitle mb-3"><span>0</span> pending</h6>
+                            <h6 className="card-subtitle mb-3"><span>{pendingbookingCount}</span> pending</h6>
                             <Link to='/vendor/view-pending-bookings'
                             ><button className='primary-btn'>View</button></Link>
                         </div>
@@ -23,7 +47,7 @@ const VendorOverviewCards = () => {
                     <div className={`${styles.card} card`}>
                         <div className="card-body text-center">
                             <h5 className="card-title">My packages</h5>
-                            <h6 className="card-subtitle mb-3"><span>0</span> Active packages</h6>
+                            <h6 className="card-subtitle mb-3"><span>{activePackagesCount}</span> Active packages</h6>
                             <button className='primary-btn'>View</button>
                         </div>
                     </div>
