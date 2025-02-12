980a51bebd24dad608b88c6575346a5cf86b72ce
Munavercv <munavermunu80@gmail.com>
2025-02-12 12:05:04 +0530

    api base url issue fixed

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,6 @@
 const config = {
-    // API_BASE_URL: "https://tripnest.xyz",
-    API_BASE_URL: "http://localhost:5000",
+    API_BASE_URL: "https://tripnest.xyz",
+    // API_BASE_URL: "http://localhost:5000",
   };
   
   export default config;
